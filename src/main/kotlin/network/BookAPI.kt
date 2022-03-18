2093c61576216a0e452850e336fbe89fb3b28163
Christian Vasquez <christianvasquezmelo@gmail.com>
2022-03-18 19:24:10 -0400

    Replace Call with suspend function

--- src/main/kotlin/network/BookAPI.kt
+++ src/main/kotlin/network/BookAPI.kt
@@ -1,12 +1,15 @@
 package network
 
 import model.Book
-import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface BookAPI {
 
     @GET("books")
-    fun all(): Call<List<Book>>
+    suspend fun all(): List<Book>
+
+    @GET("books/{id}")
+    suspend fun find(@Path("id") id: Int): Book
 
 }
