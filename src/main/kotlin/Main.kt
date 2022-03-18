abf11c5aaf8cf8e250883e34d933f53efeec6a09
Christian Vasquez <christianvasquezmelo@gmail.com>
2022-03-18 19:29:48 -0400

    Try out the API calls

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,14 @@
+import network.BookAPI
+import network.BookService
+import network.RetrofitClientProvider
+
 fun main() {
-    println("Hello world")
+    val client = RetrofitClientProvider.provide()
+    val service = BookService(client.create(BookAPI::class.java))
+
+    val book = service.find(1)
+    println(book)
+
+    val books = service.fetch()
+    books.forEach { println(it) }
 }
