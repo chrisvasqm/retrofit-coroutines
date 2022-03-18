7fe0ad3f6ab11aad8d02927ed58448466b5a9235
Christian Vasquez <christianvasquezmelo@gmail.com>
2022-03-18 19:51:03 -0400

    Refactor code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,7 +5,9 @@ import network.RetrofitClientProvider
 
 fun main() {
     val client = RetrofitClientProvider.provide()
-    val service = BookService(BookRepository(client.create(BookAPI::class.java)))
+    val api = client.create(BookAPI::class.java)
+    val repository = BookRepository(api)
+    val service = BookService(repository)
 
     val book = service.find(1)
     println(book)
